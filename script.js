d68c20c4ec9d436724b6fa5e0ad74c13a26aec83
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2023-11-13 17:46:31 +0100

    add: bonus and extra

--- script.js
+++ script.js
@@ -4,19 +4,8 @@ const app = createApp ({
     data() {
         return{
             text : "Preferisci i cani o i gatti?",
-            inputAns : "",
-            img: ""
-        }
-    },
-    methods:{
-        checkAns : function(){
-            if(inputAns.toLowerCase() === "cani"){
-                img = "https://www.petplace.com/article/dogs/breeds/dog-breeds/media_1e51607bf5488af3f6af7efbc0ac26be624866a24.jpeg?width=1200&format=pjpg&optimize=medium"
-            } else if(inputAns.toLowerCase() == "gatti") {
-                img = "https://cdn.wamiz.fr/article/images/Wamiz%20ITA/gatto-aggressivo-verso-un-altro.jpg"
-            } else {
-                img = "https://inglesedinamico.net/wp-content/uploads/2021/09/Animali-1.jpg"
-            }
+            inputAns: "" ,
+            imgS: "https://www.ilverdemondo.it/public/blog/thumbs/animali-da-cortile-quali-sono-e-come-allevarli-it-000.jpg"
         }
     }
 })
